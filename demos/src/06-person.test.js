97bac8d7d014291f05eab446aaab922f9682f7ee
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-08-26 20:30:57 -0400

    feat: metologies

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,13 +2,16 @@ const Person = require('./06-person');
 
 describe('Test Suite for Person class', () => {
   let person;
+  // Arrange
   beforeEach(() => {
     person = new Person('John Doe', 45, 1.7);
   });
 
   test('should return "down" when the result is greater than 0 and less than 18', () => {
     person.weight = 45;
+    // Act
     const result = person.calcIMC();
+    // Assert
     expect(result).toBe('down');
   });
 
